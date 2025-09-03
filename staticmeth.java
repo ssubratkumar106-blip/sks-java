bb3d1f95e5e645e23b954506c3adb74f76bb0799
Subrat Kumar Sahoo <ssubratkumar106@gmail.com>
2025-09-03 08:10:10 +0530

    new code inserted

---
+++ staticmeth.java
@@ -0,0 +1,9 @@
+class Cal {
+    static int cube(int x) {
+        return x*x*x;
+    }
+    public static void main(String a[]) {
+        int res = Cal.cube(5);
+        System.out.println(res);
+    }
+}
---
+++ thisconstarg.java
@@ -0,0 +1,16 @@
+class B{   
+  A obj;   
+  B(A obj){      this.obj=obj;    }   
+  void display(){   
+    System.out.println(obj.data); 
+    }//using data member of A class   
+class A{   
+  int data=10;   
+  A(){   
+   B b=new B(this);   
+   b.display();   
+  }   
+  public static void main(String args[]) 
+{     A a=new A();    }   
+}}
+  
